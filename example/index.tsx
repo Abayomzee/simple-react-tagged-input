86ff79c074561fa3125cf9287d850830e8d4d2e0
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-04-17 03:17:20 +0100

    🥴 fix: import dir

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
-import Tags from '../src/components/Organisms/Tags/index';
+import { ReactSimpleTaggedInput } from '../.';
 
 const App = () => {
   return (
     <div>
-      <Thing />
-      <Tags />
+      <ReactSimpleTaggedInput />
     </div>
   );
 };
