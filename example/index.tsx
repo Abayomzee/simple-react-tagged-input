b93eff45a390f4a56d8b63f9c1b504a2cb227fd7
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-04-29 03:15:52 +0100

    💫 FEAT: Tag revamping

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ReactSimpleTaggedInput } from '../.';
+import ReactSimpleTaggedInput from '../src/index';
 
 const App = () => {
   return (
     <div>
-      <ReactSimpleTaggedInput />
+      <ReactSimpleTaggedInput placeholder='You?' />
     </div>
   );
 };
