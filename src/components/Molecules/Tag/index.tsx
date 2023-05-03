9a41755d316abc9d0d0ffcbd2a74d70de2206996
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-05-04 00:21:18 +0100

    🌭 FIX: Tag close btn on hover

--- src/components/Molecules/Tag/index.tsx
+++ src/components/Molecules/Tag/index.tsx
@@ -29,8 +29,8 @@ const Tag: React.FC<Props> = props => {
     removeLabelColor,
     removeLabelClass,
     removeLabelStyle,
-    removeBtnStyle,
     removeBtnClass,
+    removeBtnStyle,
   } = props;
 
   // Data to display
