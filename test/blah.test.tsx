c7c4bfc7acf9e1a6c8fc86689ee2ab6e5767654f
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-05-24 14:03:51 +0100

    Editing Readme

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ReactSimpleTaggedInput } from '../src';
+import ReactSimpleTaggedInput from '../src/index';
 
 describe('it', () => {
   it('renders without crashing', () => {
