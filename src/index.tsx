f0b4681dd11423f07ae4646d7b743463a4bdd743
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-07-26 15:33:32 +0100

    fix: test ish

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import Tags from './components/Organisms/Tags/index';
 
-interface Props {}
+interface Props {
+  onInputChange?: any;
+  defaultData?: any;
+  autoFocus?: any;
+}
 const ReactSimpleTaggedInput: React.FC<Props> = props => {
   return <Tags {...props} />;
 };
